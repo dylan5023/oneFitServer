aa0280f75cfeaf3fd688ed5b745e5aacc83e46c0
Dylan5023 <kangjun5023@gmail.com>
2023-06-29 19:06:46 -0700

    update get all

--- server/router/result.js
+++ server/router/result.js
@@ -3,6 +3,11 @@ const router = express.Router();
 const { Result } = require("../mongoose/model");
 
 // get the result
+router.get("/result/", async (req, res) => {
+  const result = await Result.find({});
+  res.send({ result });
+});
+
 router.get("/result/:type", async (req, res) => {
   const { type } = req.params;
   const result = await Result.find({ type: type });
